3c9d6aa108ca6d63eaab641b70853cd82b2df986
Harry Yuan <harryuan.65@gmail.com>
2020-03-12 00:44:03 +0800

    nav with animation

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,11 @@
-$(document).ready(function(){
-
-})
+function toggleNav(){
+  var nav = document.getElementById("nav"),
+  toggler = document.getElementById("nav-entry");
+  if(parseInt(nav.style.width.split('px')[0]) > 0){
+    nav.style.width = '0';
+    toggler.style.color = 'rgba(54,54,54,.8)';
+  }else{
+    nav.style.width = '250px';
+    toggler.style.color = '#d8d8d8';
+  }
+}
